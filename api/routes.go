22c59f2592c092a1b00827ab624f2136ac580610
Per Buer <per.buer@gmail.com>
2021-04-26 10:49:34 +0200

    Health probe.

--- api/routes.go
+++ api/routes.go
@@ -10,6 +10,7 @@ func makeRoutes(app App) *mux.Router {
 	r.HandleFunc("/", app.HomeHandler).Methods("GET")
 	r.HandleFunc("/v1/review", app.ReviewHandler).Methods("POST")
 	r.HandleFunc("/v1/reviews", app.ReviewsHandler).Methods("GET")
+	r.HandleFunc("/healthz", app.HomeHandler).Methods("GET") // Can work as a healthz handler as well. Pings the db.
 	http.Handle("/", r)
 	return r
 }
