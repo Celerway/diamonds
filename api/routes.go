bad18a1065f766c242370b5df7967c805063dfc8
Per Buer <per.buer@gmail.com>
2021-04-26 14:31:30 +0200

    fix: add base url

--- api/routes.go
+++ api/routes.go
@@ -11,6 +11,6 @@ func makeRoutes(app App) *mux.Router {
 	r.HandleFunc("/v1/review", app.ReviewHandler).Methods("POST")
 	r.HandleFunc("/v1/reviews", app.ReviewsHandler).Methods("GET")
 	r.HandleFunc("/healthz", app.HomeHandler).Methods("GET") // Can work as a healthz handler as well. Pings the db.
-	http.Handle("/", r)
+	http.Handle("/diamonds", r)
 	return r
 }
